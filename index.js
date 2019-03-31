8b0a6e6380339aa9579a5b3b72135d0c9c0322e3
Mohammed Abdulkereem <mohammed-io@outlook.com>
2019-03-31 10:11:57 +0300

    Variables of JavaScript

---
+++ index.js
@@ -0,0 +1,14 @@
+const PI = 22/7
+
+// ..... 1000 line
+
+let r = 1;
+
+let name = 'Mohammed'
+
+let names = ["Mohammed", "Ahmed", "Farman", "Janger", "Eman"]
+
+console.log(name)
+console.log(names)
+
+console.log(PI * r ** 2);
